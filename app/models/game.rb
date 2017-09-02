190b2a91a506650b5d06641769b52a0bdb013a2d
Jacob Ela <jacob.ela@gmail.com>
2017-09-02 14:25:18 -0500

    Game validation

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
+# A Game is what brings people to a Table, it's whatever they play
 class Game < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :bgg_game_id, uniqueness: true
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,5 +3,38 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:game) { Game.new }
+  subject { game }
+
+  describe 'validation' do
+    before do
+      subject.valid?
+    end
+
+    describe 'name' do
+      it 'cannot be blank' do
+        expect(subject.errors[:name]).to include "can't be blank"
+      end
+
+      context 'when other games exist' do
+        let(:other_game) { Game.create!(name: 'Game A', bgg_game_id: '12345') }
+        let(:game) { Game.new(name: other_game.name, bgg_game_id: '54321') }
+
+        it 'must be unique' do
+          expect(subject.errors[:name]).to include 'has already been taken'
+        end
+      end
+    end
+
+    describe 'bgg_game_id' do
+      context 'when other games exist' do
+        let(:other_game) { Game.create!(name: 'Game A', bgg_game_id: '12345') }
+        let(:game) { Game.new(name: 'Game 1', bgg_game_id: other_game.bgg_game_id) }
+
+        it 'must be unique' do
+          expect(subject.errors[:bgg_game_id]).to include 'has already been taken'
+        end
+      end
+    end
+  end
 end
