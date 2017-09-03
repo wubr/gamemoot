79ec008cde06084c00401cf9e0e599a7605e4ee7
Jacob Ela <jacob.ela@gmail.com>
2017-09-02 23:08:16 -0500

    Fix other lingering Rubocop lint

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-Game.create!(name: "Memoir '44", bgg_game_id: 10630)
-Game.create!(name: 'In a Grove', bgg_game_id: 102181)
-Game.create!(name: 'Mysterium', bgg_game_id: 181304)
-Game.create!(name: 'Suburbia', bgg_game_id: 123260)
-Game.create!(name: 'Inis', bgg_game_id: 155821)
+Game.create!(name: "Memoir '44", bgg_game_id: 10_630)
+Game.create!(name: 'In a Grove', bgg_game_id: 102_181)
+Game.create!(name: 'Mysterium', bgg_game_id: 181_304)
+Game.create!(name: 'Suburbia', bgg_game_id: 123_260)
+Game.create!(name: 'Inis', bgg_game_id: 155_821)
