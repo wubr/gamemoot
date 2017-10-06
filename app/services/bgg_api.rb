46cd12c9efa0acc8cf414df7085fba4623ca43c5
Will Strinz + Jacob Ela <dev+wstrinz+wubr@bendyworks.com>
2017-10-06 11:44:39 -0500

    WIP game search for autocomplete

--- app/services/bgg_api.rb
+++ app/services/bgg_api.rb
@@ -2,14 +2,30 @@
 class BggApi
   BASE_URL = "https://www.boardgamegeek.com/xmlapi2"
   THING_URL = "#{BASE_URL}/thing"
+  SEARCH_URL = "#{BASE_URL}/search"
 
   def game_info(id)
     parsed_resp = parse_response(RestClient.get(THING_URL, params: {id: id, type: "boardgame"}).body)
     {
-      name: parsed_resp.at_css('name').try(:[], 'value'),
-      min_players: parsed_resp.at_css('minplayers').try(:[], 'value').to_i,
-      max_players: parsed_resp.at_css('maxplayers').try(:[], 'value').to_i
+      name: field_value(parsed_resp, 'name'),
+      min_players: field_value(parsed_resp, 'minplayers').to_i,
+      max_players: field_value(parsed_resp, 'maxplayers').to_i
     }
+  end
+
+  def game_search(query_hash)
+    parsed_resp = parse_response(RestClient.get(SEARCH_URL, params: {query: query_hash[:name], type: query_hash.fetch(:type, "boardgame")}).body)
+    parsed_resp.css('item').map do |i|
+      {
+        name: field_value(i, 'name'),
+        bgg_game_id: i['id'],
+        published: field_value(i, 'yearpublished'),
+      }
+    end
+  end
+
+  def field_value(parsed_resp, field)
+    parsed_resp.at_css(field).try(:[], 'value')
   end
 
   def parse_response(resp)
