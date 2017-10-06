ac4832c5ea6057d3af400249e333fd846e651526
Unknown <jacob.ela@gmail.com>
2017-10-06 08:15:36 -0500

    Show the game name in flash

--- app/controllers/game_tables_controller.rb
+++ app/controllers/game_tables_controller.rb
@@ -5,7 +5,7 @@ class GameTablesController < ApplicationController
   def create
     @game_table = GameTable.new(table_params)
     table = @game_table.table
-    game_name = @game_table.game
+    game_name = @game_table.game.name
 
     if @game_table.save
       redirect_to table, notice: "'#{game_name}' has been successfully added to the table."
