29b940f6d78f6079c11970e9175a593379afa3e0
Jacob Ela <jacob.ela@gmail.com>
2018-10-05 14:46:46 -0500

    Expose Table comments

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -69,6 +69,6 @@ class TablesController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def table_params
-    params.require(:table).permit(:name)
+    params.require(:table).permit(:name, :comments)
   end
 end
